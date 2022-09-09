8a84b8146c8330d649aa1166d9e6e16dfe35e309
李才荣 <licairong@cairongdeMacBook-Pro.local>
2022-09-09 09:05:35 +0800

    first provider

--- examples/main.tf
+++ examples/main.tf
@@ -7,7 +7,10 @@ terraform {
   }
 }
 
-provider "hashicups" {}
+provider "hashicups" {
+  username = "education"
+  password = "test123"
+}
 
 module "psl" {
   source = "./coffee"
@@ -17,4 +20,31 @@ module "psl" {
 
 output "psl" {
   value = module.psl.coffee
+}
+
+data "hashicups_order" "order" {
+  id = 1
+}
+
+output "order" {
+  value = data.hashicups_order.order
+}
+
+resource "hashicups_order" "edu" {
+  items {
+    coffee {
+      id = 3
+    }
+    quantity = 2
+  }
+  items {
+    coffee {
+      id = 2
+    }
+    quantity = 5
+  }
+}
+
+output "edu_order" {
+  value = hashicups_order.edu
 }
